2f79471e321488b6086444d8773dd8711ac06ef9
ElianMartinez10 <126831188+ElianMartinez10@users.noreply.github.com>
2023-03-07 23:49:55 -0500

    Fourth Class

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -10,21 +10,36 @@ class ObstacleManager:
     def __init__ (self):
         self.obstacles = []
 
+    def generate_obstacle(self, obstacle_type):
+        if obstacle_type == 0:
+            cactus_type = 'SMALL'
+            obstacle = Cactus(cactus_type)
+        elif obstacle_type == 1:
+            cactus_type = 'LARGE'
+            obstacle = Cactus(cactus_type)
+        else:
+            obstacle = Birds()
+        return obstacle
+
     def update(self, game):
         if len(self.obstacles) == 0:
-          obstacle_type = random.choice([SMALL_CACTUS, LARGE_CACTUS])
-          obstacle = Cactus(obstacle_type)        
+          obstacle_type = random.randint(0,2)
+          obstacle = self.generate_obstacle(obstacle_type)        
           self.obstacles.append(obstacle)
-
+        
+    
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
 
             if game.player.dino_rect.colliderect(obstacle.rect):
-                print("Colision")
-                pygame.time.delay(100)
+                pygame.time.delay(1000)
+                game.death_count += 1
                 pygame.playing = False
                 break 
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+    def reset_obstacles(self):
+        self.obstacles = []
